422f3c7e2fe499cf7b5927c0617b8cbae1d029d5
Raul Castro <castro.lab@gmail.com>
2024-01-15 21:33:50 -0500

    the template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,354 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content>
+    <meta name="author" content="Epilog Theme">
+    <title>MightShop - Multipurpose eCommerce HTML Template</title>
+
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css%3Fv=1.0.1.css">
+    <link rel="stylesheet" href="assets/css/responsive.css%3Fv=1.0.1.css">
+</head>
+
+<body>
+
+    <div class="preloader">
+        <div class="loader">
+            <div class="spinner-grow">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    </div>
+
+
+    <header class="header-area">
+        <div class="main-navbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-5 order-xs-1 col-sm-3 col-md-3 col-lg-3">
+                        <a class="navbar-brand" href="index.html">
+                            <img class="img-fluid" src="assets/images/Logo.png" alt="logo">
+                        </a>
+                    </div>
+                    <div class="col-sm-5 order-xs-2 col-md-6 col-lg-6">
+                        <form class="searchbox">
+                            <input type="search" placeholder="Search Products ...." name="search"
+                                class="searchbox-input" required>
+                            <button class="searchbox-submit" type="submit"><i class="bx bx-search-alt"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="product-listing">
+        <div class="container">
+            <div class="section-title">
+                <h3>Recent Listing Product</h3>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">5% Off</div>
+                            <img class="img-fluid" src="assets/images/products/navel_orange.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Navel
+                                    Orange</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$12.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$10.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">New</div>
+                            <img class="img-fluid" src="assets/images/products/red_tomato.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Red
+                                    Tomato</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$15.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$11.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">3% Off</div>
+                            <img class="img-fluid" src="assets/images/products/sunflower_oli.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Sunflower
+                                    Oli</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$15.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$13.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">New</div>
+                            <img class="img-fluid" src="assets/images/products/cookies_biscute.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Cookies
+                                    Biscute</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$32.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$25.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">Sale</div>
+                            <img class="img-fluid" src="assets/images/products/mackerel_fish.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Mackerel
+                                    Fish</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$32.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$25.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">20% Off</div>
+                            <img class="img-fluid" src="assets/images/products/beverage_item.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Beverage
+                                    Item</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$32.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$25.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">New</div>
+                            <img class="img-fluid" src="assets/images/products/castrated_meat.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Castrated
+                                    Meat</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$32.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$25.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3">
+                    <div class="product-item">
+                        <div class="product-image">
+                            <div class="ribbon">20% Off</div>
+                            <img class="img-fluid" src="assets/images/products/basmati_rice.png" alt="Product 1">
+                        </div>
+                        <div class="product-info">
+                            <h4 class="product-name"><a href="index.html#costumModal19" data-toggle="modal">Basmati
+                                    Rice</a></h4>
+                            <div class="row">
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="discount-price">$32.00</div>
+                                </div>
+                                <div class="col-3 col-sm-3 col-md-3 col-lg-3">
+                                    <div class="sale-price">$25.60</div>
+                                </div>
+                                <div class="col-6 col-sm-6 col-md-6 col-lg-6">
+                                    <div class="add-to-cart"><a href="index.html#"><i class="bx bx-cart-alt"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer">
+        <div class="footer-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-6 col-xl-3">
+                        <div class="footer-1">
+                            <img src="assets/images/logo-white.png" alt="Footer Logo">
+                            <ul class="social-icon">
+                                <li><a href="index.html#"><i class="bx bxl-facebook-square"></i></a></li>
+                                <li><a href="index.html#"><i class="bx bxl-twitter"></i></a></li>
+                                <li><a href="index.html#"><i class="bx bxl-linkedin-square"></i></a></li>
+                                <li><a href="index.html#"><i class="bx bxl-pinterest"></i></a></li>
+                                <li><a href="index.html#"><i class="bx bxl-youtube"></i></a></li>
+                                <li><a href="index.html#"><i class="bx bxl-google-plus"></i></a></li>
+                            </ul>
+                            <div class="contact-info">
+                                <p><strong>Hotline: </strong>16768</p>
+                                <p><strong>Phone: </strong>(+123) 456-657878</p>
+                                <p><strong>Email: </strong><a href="../../../cdn-cgi/l/email-protection.html"
+                                        class="__cf_email__"
+                                        data-cfemail="c9a0a7afa689acb1a8a4b9a5ace7aaa6a4">[email&#160;protected]</a>
+                                </p>
+                                <p><strong>Address: </strong>6757 Blvd, The Bronx, NY 1058, USA</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-xl-3">
+                        <div class="footer-2">
+                            <h4 class="footer-title">Pages</h4>
+                            <ul class="footer-menu">
+                                <li><a href="about.html"><i class="bx bx-radio-circle"></i> About Us</a></li>
+                                <li><a href="shop.html"><i class="bx bx-radio-circle"></i> Shop</a></li>
+                                <li><a href="blog.html"><i class="bx bx-radio-circle"></i> Blog</a></li>
+                                <li><a href="error-404.html"><i class="bx bx-radio-circle"></i> 404 Error</a></li>
+                                <li><a href="coming-soon.html"><i class="bx bx-radio-circle"></i> Coming Soon</a>
+                                </li>
+                                <li><a href="faqs.html"><i class="bx bx-radio-circle"></i> FAQ's</a></li>
+                                <li><a href="contact.html"><i class="bx bx-radio-circle"></i> Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-xl-3">
+                        <div class="footer-2">
+                            <h4 class="footer-title">Support</h4>
+                            <ul class="footer-menu">
+                                <li><a href="index.html#"><i class="bx bx-radio-circle"></i> Help & FAQs</a></li>
+                                <li><a href="index.html#"><i class="bx bx-radio-circle"></i> Knowledge-Based</a>
+                                </li>
+                                <li><a href="index.html#"><i class="bx bx-radio-circle"></i> Payment System</a></li>
+                                <li><a href="index.html#"><i class="bx bx-radio-circle"></i> Privacy Policy</a></li>
+                                <li><a href="index.html#"><i class="bx bx-radio-circle"></i> Terms & Conditions</a>
+                                </li>
+                                <li><a href="index.html#"><i class="bx bx-radio-circle"></i> Delivery
+                                        Information</a></li>
+                                <li><a href="index.html#"><i class="bx bx-radio-circle"></i> Order and Returns</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-xl-3">
+                        <div class="footer-2">
+                            <h4 class="footer-title">Subscribe</h4>
+                            <p>Subscrive to our newsletter, so that you can be the first about new product offer.</p>
+                            <form action="index.html" method="post">
+                                <input type="text" class="form-control" placeholder="Enter Your Email Address"
+                                    name="subscribe">
+                                <button type="submit" class="btn btn-secondary">Subscribe Now</button>
+                            </form>
+                            <img class="img-fluid mt-2" src="assets/images/we-accept-payment.png" alt="We Accept">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="footer-copyright">
+                            <p class="copyright-text">Copyright &copy; 2020 MightShop. Designed By <a
+                                    href="index.html#">Epilog Theme</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="top-bottom-scroll"><i class="bx bx-chevrons-up bx-fade-up"></i></div>
+
+    <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/velocity.min.js"></script>
+    <script src="assets/js/velocity.ui.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/main.js%3Fv=1.0.0"></script>
+</body>
+
+</html>
